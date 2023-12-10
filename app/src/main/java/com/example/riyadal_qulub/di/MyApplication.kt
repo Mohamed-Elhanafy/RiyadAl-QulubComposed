1168327cb6392da9d46ce607f4154141f7b2c690
Mohamed-Elhanafy <amohamed8684@gmail.com>
2023-12-10 10:19:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/riyadal_qulub/di/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.example.riyadal_qulub.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
---
+++ app/src/main/java/com/example/riyadal_qulub/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.riyadal_qulub.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+val Primary = Color(0xFF6750A4)
+val PrimaryVariant = Color(0xFF8F52AC)
+val Gold = Color(0xFFE9A12C)
+val Secondary = Color(0xFFEBDEFF)
