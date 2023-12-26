a725ae75e79f56bfffa6000737256c1b9a376e24
Mohamed Elhanafy <amohamed8684@gmail.com>
2023-12-26 10:51:41 +0200

    -adding AnimatedCircularProgressIndicator

---
+++ app/src/main/java/com/example/riyadal_qulub/ui/components/WeeklyProgress.kt
@@ -0,0 +1,138 @@
+package com.example.riyadal_qulub.ui.components
+
+import AnimatedCircularProgressIndicator
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.riyadal_qulub.ui.theme.Primary
+import com.example.riyadal_qulub.ui.theme.Purple40
+import com.example.riyadal_qulub.ui.theme.Purple80
+import com.example.riyadal_qulub.ui.theme.PurpleGrey40
+import com.example.riyadal_qulub.ui.theme.rubikSansFamily
+
+
+@Composable
+fun WeeklyProgress(weekPrev: List<Float>) {
+    Box {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(4.dp),
+            colors = CardDefaults.cardColors(Color.White)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "نظرة عامة علي إنتاجية الأسبوع",
+                    textAlign = TextAlign.Right,
+                    fontFamily = rubikSansFamily,
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
+
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    AnimatedCircularProgressIndicator(
+                        currentValue = 17,
+                        maxValue = 20,
+                        progressBackgroundColor = Purple80,
+                        progressIndicatorColor = Primary,
+                        day = "السبت",
+                        completedColor = Primary
+                    )
+
+                    AnimatedCircularProgressIndicator(
+                        currentValue = 17,
+                        maxValue = 20,
+                        progressBackgroundColor = Purple80,
+                        progressIndicatorColor = Primary,
+                        day = "الأحد",
+                        completedColor = Primary
+                    )
+
+                    AnimatedCircularProgressIndicator(
+                        currentValue = 17,
+                        maxValue = 20,
+                        progressBackgroundColor = Purple80,
+                        progressIndicatorColor = Primary,
+                        day = "الأثنين",
+                        completedColor = Primary
+                    )
+
+                    AnimatedCircularProgressIndicator(
+                        currentValue = 17,
+                        maxValue = 20,
+                        progressBackgroundColor = Purple80,
+                        progressIndicatorColor = Primary,
+                        day = "الثلاثاء",
+                        completedColor = Primary
+                    )
+
+
+                    AnimatedCircularProgressIndicator(
+                        currentValue = 17,
+                        maxValue = 20,
+                        progressBackgroundColor = Purple80,
+                        progressIndicatorColor = Primary,
+                        day = "الأربعاء",
+                        completedColor = Primary
+                    )
+
+
+                    AnimatedCircularProgressIndicator(
+                        currentValue = 17,
+                        maxValue = 20,
+                        progressBackgroundColor = Purple80,
+                        progressIndicatorColor = Primary,
+                        day = "الخميس",
+                        completedColor = Primary
+                    )
+
+
+                    AnimatedCircularProgressIndicator(
+                        currentValue = 17,
+                        maxValue = 20,
+                        progressBackgroundColor = Purple80,
+                        progressIndicatorColor = Primary,
+                        day = "الجمعة",
+                        completedColor = Primary
+                    )
+
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun show() {
+    WeeklyProgress(weekPrev = listOf(1f, 2f, 3f, 4f, 5f, 6f, 7f))
+
+}
--- app/src/main/java/com/example/riyadal_qulub/ui/screens/statisticsScreen/StatisticsScreen.kt
+++ app/src/main/java/com/example/riyadal_qulub/ui/screens/statisticsScreen/StatisticsScreen.kt
@@ -3,6 +3,7 @@ package com.example.riyadal_qulub.ui.screens.statisticsScreen
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.riyadal_qulub.ui.components.WeeklyProgress
 import com.example.riyadal_qulub.ui.screens.wirdScreen.WirdViewModel
 
 private const val TAG = "StatisticsScreen"
@@ -12,5 +13,5 @@ fun StatisticsScreen(
     viewModel: StatisticsViewModel = hiltViewModel(),
     navController: NavController
 ) {
-
+    WeeklyProgress(weekPrev = listOf(1f, 2f, 3f, 4f, 5f, 6f, 7f))
 }
